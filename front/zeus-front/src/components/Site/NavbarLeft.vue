3c608aa080c656d489a6624fe13f5e9e173d2def
Sandro Souza <sandro@greenn.com.br>
2024-08-26 21:35:12 -0300

    logo navbar

--- front/zeus-front/src/components/Site/NavbarLeft.vue
+++ front/zeus-front/src/components/Site/NavbarLeft.vue
@@ -4,5 +4,10 @@
 
 <template>
   <v-app-bar-nav-icon></v-app-bar-nav-icon>
-  <h1>Zeus</h1>
+  <v-img
+    :width="200"
+    aspect-ratio="16/9"
+    cover
+    src="/src/assets/logo.png"
+  ></v-img>
 </template>
